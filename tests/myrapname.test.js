02cad465af31f76aa7e5d1295e78ce934dffd210
Kristi Miller <kristi.ann.miller@gmail.com>
2021-06-03 10:49:18 -0600

    Add playwright setup and test hooks

--- tests/myrapname.test.js
+++ tests/myrapname.test.js
@@ -1,4 +1,23 @@
+const { chromium } = require('playwright')
+
 describe("Myrapname.com", () => {
+  let browser
+  let page
+
+  beforeAll(async () => {
+    browser = await chromium.launch({headless: false, slowMo: 50})
+    page = await browser.newPage()
+
+    if(!page) throw new Error("No Connection Established")
+
+    await page.goto("https://www.myrapname.com", {
+      waitUntil: "networkidle0"
+    });
+  });
+
+  afterAll(async () => {
+    await browser.close();
+  });
 
   it("should exist", async () => {
     await page.goto("https://www.myrapname.com");
